f9178619b2544da0787e1c7e7567fe2dc891db64
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-03-15 09:40:48 +0530

    Time: 16 ms (97.41%), Space: 11.1 MB (60.66%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -4,14 +4,15 @@ public:
         stack<int>st;
         int n=s.length();
         int m=0;
+        int k=0;
         for (int i=0;i<n;i++){
             if (s[i]=='('){
-                st.push(s[i]);
+                k++;
             }
             else if (s[i]==')'){
-                if (!st.empty()){
-                    st.pop();
+                if (k){
                     m++;
+                    k--;
                 }
             }
         }
