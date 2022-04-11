74463cdab6818d6842e69cb22060b06bdfb8e08d
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-04-11 20:06:12 +0530

    Time: 20 ms (67.95%), Space: 11.9 MB (54.66%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int change(int a, vector<int>& coins) {
+        int n=coins.size();
+        int t[n+1][a+1];
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=a;j++){
+                if (j==0){
+                  t[i][j]=1;  
+                }
+                else if (i==0){
+                    t[i][j]=0;
+                }
+            }
+        }
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=a;j++){
+                if (coins[i-1]<=j){
+                    t[i][j]=t[i][j-coins[i-1]]+t[i-1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][a];
+    }
+};
