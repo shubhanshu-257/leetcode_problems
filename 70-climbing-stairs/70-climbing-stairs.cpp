af96f188755655c1162f41f9b98769d7f73ccf37
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-04-17 17:13:13 +0530

    Time: 3 ms (27.49%), Space: 5.9 MB (82.94%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[50];
+    int solve(int n){
+        if (n<=1){
+            return 1;
+        }
+        if (dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
