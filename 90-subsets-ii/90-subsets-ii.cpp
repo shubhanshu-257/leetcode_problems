8ca243554585567949214c1126acd31485e336ca
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-05-10 07:21:16 +0530

    Time: 7 ms (43.56%), Space: 8 MB (47.15%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    void f(int ind,vector<int>nums,vector<int>op,vector<vector<int>>&ans){
+       ans.push_back(op);
+        for (int i=ind;i<nums.size();i++){
+            if (i!=ind && nums[i]==nums[i-1])continue;
+            op.push_back(nums[i]);
+            f(i+1,nums,op,ans);
+            op.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>op;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        f(0,nums,op,ans);
+        return ans;
+    }
+};
