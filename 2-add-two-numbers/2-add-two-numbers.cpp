27e15b8c5fe21ef465373c5c6218b632c9195c80
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-06-07 08:25:55 +0530

    Time: 59 ms (31.85%), Space: 71.4 MB (81.39%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,64 +11,78 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1=l1;
-        ListNode* t2=l2;
-        ListNode* t3=l2;
+        ListNode* t1=NULL;
+        ListNode* t2=NULL;
         int x=0;
-        ListNode* q=l1;
-        while(t1!=NULL && t2!=NULL){
-            int y = t1->val + t2->val +x;
-            if (y>9){
-                t1->val=y%10;
-                x=1;
+        while(l1!=NULL && l2!=NULL){
+            ListNode* t=new ListNode(0);
+            int m=l1->val+l2->val+x;
+            //cout<<m;
+            
+            x=m/10;
+            m=m%10;
+            t->val=m;
+            if (t1){
+                t1->next=t;
+                t1=t1->next;
             }
             else{
-                t1->val=y%10;
-                x=0;
+                t1=t;
+                t2=t;
             }
-             q=t1;
-            t1=t1->next;
-            t2=t2->next;
-           
+            l1=l1->next;
+            l2=l2->next;
         }
-        while(t1!=NULL){
-            int y=t1->val+x;
-            if (y>9){
-            t1->val=y%10;
-                x=1;}
+        while(l1!=NULL){
+            ListNode* t=new ListNode(0);
+            int m=l1->val+x;
+            
+            x=m/10;
+            m=m%10;
+            t->val=m;
+            if (t1){
+                t1->next=t;
+                t1=t1->next;
+            }
             else{
-                t1->val=y%10;
-                x=0;
-            }q=t1;
-            t1=t1->next;
+                t1=t;
+                t2=t;
+            }
+            l1=l1->next;
+        }
+        while(l2!=NULL){
+            ListNode* t=new ListNode(0);
+            int m=l2->val+x;
+            
+            x=m/10;
+            m=m%10;
+            t->val=m;
+            if (t1){
+                t1->next=t;
+                t1=t1->next;
+            }
+            else{
+                t1=t;
+                t2=t;
+            }
+            l2=l2->next;
+        }
+        if (x!=0){
+            ListNode* t=new ListNode(0);
+            int m=x;
+            m=m%10;
+            x=m/10;
+            t->val=m;
+            if (t1){
+                t1->next=t;
+                t1=t1->next;
+            }
+            else{
+                t1=t;
+                t2=t;
+            }
             
         }
-        ListNode* p=NULL;
-        while(t2!=NULL){
-            int y=t2->val+x;
-            if (y>9){
-            t3->val=y%10;
-                x=1;}
-            else{
-                t3->val=y%10;
-                x=0;
-            }
-            p=t3;
-            t3=t3->next;
-            t2=t2->next;
-        }
-        if (x==1){
-            t3->val=x;
-            t3->next=NULL;
-            q->next=l2;
-        }
-        else{
-            if (p!=NULL){
-                p->next=NULL;
-                q->next=l2;
-            }
-            
-        }
-        return l1;
+        return t2;
     }
 };
