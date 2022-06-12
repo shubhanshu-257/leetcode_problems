67c93169233f1be9a949bde5cf79c112dc180073
shubhanshu-257 <93247904+shubhanshu-257@users.noreply.github.com>
2022-06-12 13:14:28 +0530

    Time: 32 ms (55.92%), Space: 13.7 MB (37.53%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    bool bip(vector<vector<int>>& graph,vector<int>vis,int s){
+    bool bfs(vector<vector<int>>& graph,vector<int>&vis,int s,vector<int>&color){
         queue<int>q;
+        int col=0;
         q.push(s);
         vis[s]=1;
+        color[s]=0;
         while(!q.empty()){
-            int x= q.front();
+            int x=q.front();
             q.pop();
             for (int i=0;i<graph[x].size();i++){
-                if (vis[graph[x][i]]==-1){
-                    vis[graph[x][i]]=1-vis[x];
+                if (vis[graph[x][i]]==0){
                     q.push(graph[x][i]);
+                    color[graph[x][i]]=color[x]-1;
+                    vis[graph[x][i]]=1;
                 }
-                else if (vis[graph[x][i]]==vis[x]) return false;
+                else if (color[graph[x][i]]==color[x]){
+                    return false;
+                }
             }
         }
         return true;
@@ -20,12 +25,14 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>vis(n);
-        for (int i=0;i<n;i++){
-            vis[i]=-1;
-        }
-        for (int i=0;i<n;i++){
-            if (vis[i]==-1){
-                if (!bip(graph,vis,i))return false;
+        vector<int>color(n);
+        bool c=true;
+        for (int i=0;i<graph.size();i++){
+            if (vis[i]==0){
+               c= bfs(graph,vis,i,color);
+            }
+            if (c==false){
+                return c;
             }
         }
         return true;
